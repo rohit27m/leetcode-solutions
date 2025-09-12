3ed38df2acf8fbe1e115b0ac06eff1f4d9cb894d
rohit27m <23951a057q@iare.ac.in>
2025-09-12 19:56:10 +0530

    Time: 11 ms (34.12%), Space: 45.5 MB (82.68%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean doesAliceWin(String s) {
         for (char character : s.toCharArray()) {
-            if ("aeiou".indexOf(character) >= 0) return true;
+            if ("aeiou".indexOf(character) >= 0){
+                return true;
+            }
         }
         return false;
     }
